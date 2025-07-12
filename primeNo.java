c9f2d90033c7d761742c9542ee69a24b02c82d3a
soumadip <soumadipghosh01@gmail.com>
2025-07-13 00:50:52 +0530

    new

--- primeNo.java
+++ primeNo.java
@@ -16,7 +16,7 @@ public class primeNo {
         if(num<1){
             return false;
         }
-        for(int i =2;i<num;i++){
+        for(int i =2;i< Math.sqrt(num);i++){
             if(num%i==0){
                 return false;
             }
